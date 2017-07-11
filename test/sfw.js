15fa5be2a4267ef9aefe3510bd86ae396c0903db
Ash Wilson <smashwilson@github.com>
2017-07-11 15:05:10 -0400

    Test expected configure() behavior

--- test/sfw.js
+++ test/sfw.js
@@ -1,7 +1,29 @@
 const sfw = require('../lib')
 
+const path = require('path')
+const fs = require('mz/fs')
+
 describe('entry point', function () {
-  it('calls a C++ function correctly', function () {
-    assert.equal(sfw.ok(), 'whatever')
+  let mainLogFile
+
+  beforeEach(function () {
+    mainLogFile = path.join(__dirname, '..', 'main.log')
+  })
+
+  afterEach(async function () {
+    await fs.unlink(mainLogFile).catch(() => {})
+  })
+
+  describe('configuration', function () {
+    it('validates its arguments', function () {
+      assert.throws(() => sfw.configure(), 'requires an option object')
+    })
+
+    it('configures the main thread logger', async function () {
+      sfw.configure({mainLogFile})
+
+      const contents = await fs.readFile(mainLogFile)
+      assert.match(contents, /FileLogger opened/)
+    })
   })
 })
