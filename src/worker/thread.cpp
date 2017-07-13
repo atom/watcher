a7a9eeffb4adc4e02e2e82070778f08b42376875
Ash Wilson <smashwilson@github.com>
2017-07-13 08:40:58 -0400

    Use the thread helper in the WorkerThread and stub out event handling

--- src/worker/thread.cpp
+++ src/worker/thread.cpp
@@ -1,30 +1,89 @@
+#include <vector>
+#include <memory>
 #include <iostream>
+#include <utility>
 #include <uv.h>
 
 #include "thread.h"
 #include "../log.h"
 #include "../queue.h"
+#include "../event.h"
 
 using std::endl;
+using std::vector;
+using std::unique_ptr;
+using std::move;
 
-void start_helper(void *arg)
+WorkerThread::WorkerThread(Queue &in, Queue &out, uv_async_t *main_callback) :
+  in{in},
+  out{out},
+  main_callback{main_callback},
+  start_callback{this, &WorkerThread::work}
 {
-  WorkerThread *worker = (WorkerThread*) arg;
-  worker->start();
+  //
 }
 
-WorkerThread::WorkerThread(Queue &in, Queue &out) :
-  in{in}, out{out}
+void WorkerThread::run()
 {
   int err;
 
-  err = uv_thread_create(&thread, start_helper, (void*) this);
+  err = start_callback.create_thread(&thread);
   report_uv_error(err);
 }
 
-void WorkerThread::start()
+void WorkerThread::work()
 {
   Logger::toFile("worker.log");
 
   LOGGER << "What's up" << endl;
+}
+
+void WorkerThread::handle_events()
+{
+  LOGGER << "Handling events." << endl;
+
+  unique_ptr<vector<Event>> accepted = in.accept_all();
+  if (!accepted) {
+    LOGGER << "No events waiting." << endl;
+    return;
+  }
+
+  LOGGER << "Handling " << accepted->size() << " events." << endl;
+  vector<Event> acks;
+  acks.reserve(accepted->size());
+
+  for (auto it = accepted->begin(); it != accepted->end(); ++it) {
+    CommandEvent *command = it->as_command();
+    if (!command) {
+      LOGGER << "Received unexpected event " << *it << "." << endl;
+      continue;
+    }
+
+    switch (command->get_action()) {
+      case COMMAND_ADD:
+        LOGGER << "Adding watcher for path " << command->get_root() << "." << endl;
+        break;
+      case COMMAND_REMOVE:
+        LOGGER << "Removing watcher for path " << command->get_root() << "." << endl;
+        break;
+      case COMMAND_LOG_FILE:
+        Logger::toFile(command->get_root().c_str());
+        break;
+      case COMMAND_LOG_DISABLE:
+        LOGGER << "Disabling logger." << endl;
+        Logger::disable();
+        break;
+      default:
+        LOGGER << "Received command with unexpected action " << *it << "." << endl;
+        break;
+    }
+
+    AckEvent ack(&*it);
+    Event response(move(ack));
+    acks.push_back(move(response));
+  }
+
+  LOGGER << "Replying with " << acks.size() << " acks." << endl;
+  out.enqueue_all(acks.begin(), acks.end());
+  uv_async_send(main_callback);
 }
--- src/worker/thread.h
+++ src/worker/thread.h
@@ -4,22 +4,26 @@
 #include <uv.h>
 
 #include "../queue.h"
+#include "../callbacks.h"
 #include "../errable.h"
 
 class WorkerThread : public Errable {
 public:
-  WorkerThread(Queue &in, Queue &out);
+  WorkerThread(Queue &in, Queue &out, uv_async_t *main_callback);
+
+  void run();
 
 private:
-  void start();
+  void work();
+  void handle_events();
 
   Queue &in;
   Queue &out;
 
-  uv_async_t main_callback;
-  uv_thread_t thread;
+  uv_async_t *main_callback;
 
-  friend void start_helper(void *arg);
+  ThreadCallback start_callback;
+  uv_thread_t thread;
 };
 
 #endif
