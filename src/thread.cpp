44aec89904d5a15e5a50b89ab53447dcbddffce7
Ash Wilson <smashwilson@github.com>
2017-07-14 14:42:37 -0400

    Change "callbacks" to "thread"

---
+++ src/thread.cpp
@@ -0,0 +1,64 @@
+#include <memory>
+#include <functional>
+#include <vector>
+#include <utility>
+#include <uv.h>
+
+#include "thread.h"
+#include "message.h"
+
+#include <iostream>
+#include <iomanip>
+
+using std::function;
+using std::unique_ptr;
+using std::vector;
+using std::move;
+
+void thread_callback_helper(void *arg)
+{
+  std::cout << "About to dispatch to member function " << std::hex << arg << std::endl;
+
+  function<void()> *bound_fn = (std::function<void()>*) arg;
+  (*bound_fn)();
+
+  std::cout << "After member function dispatch" << std::endl;
+}
+
+void Thread::run()
+{
+  int err;
+
+  err = uv_thread_create(&uv_handle, thread_callback_helper, &work_fn);
+  report_uv_error(err);
+}
+
+void Thread::send(Message &&message)
+{
+  if (!is_healthy()) return;
+  in.enqueue(move(message));
+  wake();
+}
+
+unique_ptr<vector<Message>> Thread::receive_all()
+{
+  if (!is_healthy()) return nullptr;
+  return out.accept_all();
+}
+
+void Thread::emit(Message &&message)
+{
+  if (!is_healthy()) return;
+  out.enqueue(move(message));
+}
+
+unique_ptr<vector<Message>> Thread::process_all()
+{
+  if (!is_healthy()) return nullptr;
+  return in.accept_all();
+}
+
+void Thread::wake()
+{
+  //
+}
---
+++ src/thread.h
@@ -0,0 +1,63 @@
+#ifndef THREAD_H
+#define THREAD_H
+
+#include <memory>
+#include <functional>
+#include <vector>
+#include <uv.h>
+
+#include "errable.h"
+#include "queue.h"
+#include "message.h"
+
+#include <iostream>
+
+void thread_callback_helper(void *arg);
+
+class Thread : Errable {
+public:
+  template< class T >
+  Thread(T* self, void (T::*fn)()) :
+    work_fn{std::bind(std::mem_fn(fn), self)}
+  {
+    std::cout << "Thread::Thread constructor" << std::endl;
+  };
+
+  void run();
+
+  void send(Message &&message);
+
+  template <class InputIt>
+  void send_all(InputIt begin, InputIt end)
+  {
+    if (!is_healthy()) return;
+    in.enqueue_all(begin, end);
+    wake();
+  }
+
+  std::unique_ptr<std::vector<Message>> receive_all();
+
+protected:
+  virtual void wake();
+
+  void emit(Message &&message);
+
+  template <class InputIt>
+  void emit_all(InputIt begin, InputIt end)
+  {
+    if (!is_healthy()) return;
+    out.enqueue_all(begin, end);
+    wake();
+  }
+
+  std::unique_ptr<std::vector<Message>> process_all();
+
+private:
+  Queue in;
+  Queue out;
+
+  uv_thread_t uv_handle;
+  std::function<void()> work_fn;
+};
+
+#endif
