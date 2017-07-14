55d00423a94744638817bc4f6ad4905b5faaeeed
Ash Wilson <smashwilson@github.com>
2017-07-14 14:52:39 -0400

    Invoke a callback on configuration ack (and Command acks in general)

--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,42 @@
 #include <string>
 #include <sstream>
 #include <functional>
+#include <utility>
+#include <unordered_map>
+#include <vector>
 #include <nan.h>
 #include <v8.h>
 #include <uv.h>
 
 #include "log.h"
 #include "queue.h"
-#include "callbacks.h"
-#include "worker/thread.h"
+#include "worker/worker_thread.h"
 
-using namespace v8;
+using v8::Local;
+using v8::Value;
+using v8::Object;
+using v8::String;
+using v8::Function;
+using v8::FunctionTemplate;
 using std::unique_ptr;
 using std::string;
 using std::ostringstream;
 using std::endl;
+using std::unordered_map;
+using std::vector;
+using std::move;
+using std::make_pair;
 
 static void handle_events_helper(uv_async_t *handle);
 
 class Main {
 public:
-  Main() : worker_thread{out, in, &event_handler}
+  Main() : worker_thread{&event_handler}
   {
     int err;
+
+    next_command_id = 0;
+
     err = uv_async_init(uv_default_loop(), &event_handler, handle_events_helper);
     if (err) return;
 
@@ -36,17 +50,68 @@ public:
     Logger::to_file(main_log_file.c_str());
   }
 
+  void use_worker_log_file(string &&worker_log_file, unique_ptr<Nan::Callback> callback)
+  {
+    CommandID command_id = next_command_id;
+
+    CommandPayload command_payload(next_command_id, COMMAND_LOG_FILE, move(worker_log_file));
+    Message log_file_message(move(command_payload));
+
+    pending_callbacks.emplace(command_id, move(callback));
+
+    next_command_id++;
+
+    LOGGER << "Sending command " << log_file_message << " to worker thread." << endl;
+    worker_thread.send(move(log_file_message));
+  }
+
   void handle_events()
   {
-    LOGGER << "Handling events." << endl;
+    Nan::HandleScope scope;
+
+    LOGGER << "Handling messages from the worker thread." << endl;
+
+    unique_ptr<vector<Message>> accepted = worker_thread.receive_all();
+    if (!accepted) {
+      LOGGER << "No messages waiting." << endl;
+      return;
+    }
+
+    LOGGER << accepted->size() << " messages to process." << endl;
+
+    for (auto it = accepted->begin(); it != accepted->end(); ++it) {
+      const AckPayload *ack_message = it->as_ack();
+      if (ack_message) {
+        auto maybe_callback = pending_callbacks.find(ack_message->get_key());
+        if (maybe_callback == pending_callbacks.end()) {
+          LOGGER << "Ignoring unexpected ack " << *it << endl;
+          continue;
+        }
+
+        unique_ptr<Nan::Callback> callback = move(maybe_callback->second);
+        pending_callbacks.erase(maybe_callback);
+
+        callback->Call(0, nullptr);
+        continue;
+      }
+
+      const FileSystemPayload *filesystem_message = it->as_filesystem();
+      if (filesystem_message) {
+        LOGGER << "Received filesystem event message " << *it << endl;
+        continue;
+      }
+
+      LOGGER << "Received unexpected message " << *it << endl;
+    }
   }
 
 private:
-  Queue in;
-  Queue out;
   uv_async_t event_handler;
 
   WorkerThread worker_thread;
+
+  CommandID next_command_id;
+  unordered_map<CommandID, unique_ptr<Nan::Callback>> pending_callbacks;
 };
 
 static Main instance;
@@ -94,6 +159,7 @@ void configure(const Nan::FunctionCallbackInfo<Value> &info)
 {
   string main_log_file;
   string worker_log_file;
+  bool async = false;
 
   Nan::MaybeLocal<Object> maybe_options = Nan::To<Object>(info[0]);
   if (maybe_options.IsEmpty()) {
@@ -105,14 +171,20 @@ void configure(const Nan::FunctionCallbackInfo<Value> &info)
   if (!get_string_option(options, "mainLogFile", main_log_file)) return;
   if (!get_string_option(options, "workerLogFile", worker_log_file)) return;
 
-  Nan::Callback *callback = new Nan::Callback(info[1].As<Function>());
+  unique_ptr<Nan::Callback> callback(new Nan::Callback(info[1].As<Function>()));
 
   if (!main_log_file.empty()) {
     instance.use_main_log_file(move(main_log_file));
   }
 
-  callback->Call(0, 0);
-  delete callback;
+  if (!worker_log_file.empty()) {
+    instance.use_worker_log_file(move(worker_log_file), move(callback));
+    async = true;
+  }
+
+  if (!async) {
+    callback->Call(0, 0);
+  }
 }
 
 void watch(const Nan::FunctionCallbackInfo<Value> &info)
