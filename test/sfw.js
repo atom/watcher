ecce49f755f49a958de8f672d2284e443edf97ba
Ash Wilson <smashwilson@github.com>
2017-07-17 16:29:47 -0400

    Basic unwatch() test

--- test/sfw.js
+++ test/sfw.js
@@ -121,7 +121,38 @@ describe('entry point', function () {
   })
 
   describe('unwatching a directory', function () {
-    it('unwatches a previously watched directory')
+    beforeEach(async function () {
+      await sfw.configure({mainLogFile, workerLogFile})
+    })
+
+    it('unwatches a previously watched directory', async function () {
+      let error = null
+      const events = []
+
+      const sub = await sfw.watch(watchDir, (err, es) => {
+        error = err
+        events.push(...es)
+      })
+      subs.push(sub)
+
+      const filePath = path.join(watchDir, 'file.txt')
+      await fs.writeFile(filePath, 'original')
+
+      await until('the event arrives', () => events.some(event => event.oldPath === filePath))
+      const eventCount = events.length
+      assert.isNull(error)
+
+      await sub.unwatch()
+
+      await fs.writeFile(filePath, 'the modification')
+
+      // Give the modification event a chance to arrive.
+      // Not perfect, but adequate.
+      await new Promise(resolve => setTimeout(resolve, 100))
+
+      assert.lengthOf(events, eventCount)
+    })
+
     it('is a no-op if the directory is not being watched')
   })
 })
