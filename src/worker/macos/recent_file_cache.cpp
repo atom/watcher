bf2a6c2195b08cb81f3a0273f373d5f143ed3cc2
Ash Wilson <smashwilson@github.com>
2017-07-21 13:05:27 -0400

    Rework the RecentFileCache to cache more things

--- src/worker/macos/recent_file_cache.cpp
+++ src/worker/macos/recent_file_cache.cpp
@@ -2,60 +2,100 @@
 
 #include <string>
 #include <chrono>
-#include <unordered_set>
+#include <unordered_map>
 #include <map>
+#include <memory>
+#include <utility>
+#include <sys/stat.h>
+
+#include "../../log.h"
 
 using std::string;
-using std::unordered_set;
+using std::endl;
+using std::move;
+using std::shared_ptr;
 using std::multimap;
 using std::chrono::steady_clock;
 using std::chrono::time_point;
-using std::chrono::duration;
-using std::chrono::seconds;
+using std::chrono::minutes;
 
-void RecentFileCache::does_exist(const string &path, const EntryKind &entryKind)
+// If the cache contains more than this many entries, any entries older than CACHE_AGEOFF will be purged.
+static const size_t CACHE_WATERMARK = 4096;
+
+// Entries older than this duration will be purged once the cache grows beyond CACHE_WATERMARK entries.
+static const minutes CACHE_AGEOFF = minutes(5);
+
+CacheEntry::CacheEntry(std::string path, EntryKind entry_kind, ino_t inode, off_t size) :
+  path{path},
+  entry_kind{entry_kind},
+  inode{inode},
+  size{size},
+  last_seen{steady_clock::now()}
 {
-  bool inserted = seen_paths.insert({path, entryKind}).second;
-  if (inserted) {
-    time_point<steady_clock> ts = steady_clock::now();
-    by_timestamp.insert({ts, path});
-  }
+  //
+}
+
+void RecentFileCache::does_exist(const string &path, EntryKind entry_kind, ino_t inode, off_t size)
+{
+  shared_ptr<CacheEntry> entry(new CacheEntry(path, entry_kind, inode, size));
+
+  // Clear an existing entry if one exists
+  does_not_exist(path);
+
+  by_path.insert({path, entry});
+  by_inode.insert({inode, entry});
+  by_timestamp.insert({entry->last_seen, entry});
 }
 
 void RecentFileCache::does_not_exist(const string &path)
 {
-  seen_paths.erase(path);
-}
+  auto maybe = by_path.find(path);
+  if (maybe != by_path.end()) {
+    shared_ptr<CacheEntry> existing = maybe->second;
 
-bool RecentFileCache::has_been_seen(const string &path, const EntryKind &entryKind)
-{
-  auto maybe = seen_paths.find(path);
-  if (maybe == seen_paths.end()) {
-    return false;
-  } else {
-    return maybe->second == entryKind;
+    by_inode.erase(existing->inode);
+    by_timestamp.erase(existing->last_seen);
+    by_path.erase(maybe);
   }
 }
 
-EntryKind RecentFileCache::last_known_kind(const string &path)
+shared_ptr<CacheEntry> RecentFileCache::at_path(const string &path)
 {
-  auto maybe_entry_kind = seen_paths.find(path);
-  if (maybe_entry_kind == seen_paths.end()) {
-    return KIND_UNKNOWN;
-  }
-  return maybe_entry_kind->second;
+  auto maybe = by_path.find(path);
+  return maybe != by_path.end() ? maybe->second : nullptr;
+}
+
+shared_ptr<CacheEntry> RecentFileCache::at_inode(const ino_t inode)
+{
+  auto maybe = by_inode.find(inode);
+  return maybe != by_inode.end() ? maybe->second : nullptr;
 }
 
 void RecentFileCache::purge()
 {
-  time_point<steady_clock> oldest = steady_clock::now() - seconds(5);
+  if (by_path.size() <= CACHE_WATERMARK) {
+    return;
+  }
+
+  LOGGER << "Cache currently contains " << plural(by_path.size(), "entry", "entries")
+    << ". Pruning triggered." << endl;
+
+  time_point<steady_clock> oldest = steady_clock::now() - CACHE_AGEOFF;
 
   auto to_keep = by_timestamp.upper_bound(oldest);
+  size_t prune_count = 0;
   for (auto it = by_timestamp.begin(); it != to_keep && it != by_timestamp.end(); ++it) {
-    seen_paths.erase(it->second);
+    shared_ptr<CacheEntry> entry = it->second;
+    by_path.erase(entry->path);
+    by_inode.erase(entry->inode);
+
+    prune_count++;
   }
 
   if (to_keep != by_timestamp.begin()) {
     by_timestamp.erase(by_timestamp.begin(), to_keep);
   }
+
+  LOGGER << "Pruned " << plural(prune_count, "entry", "entries") << ". "
+    << plural(by_path.size(), "entry", "entries") << " remain." << endl;
 }
--- src/worker/macos/recent_file_cache.h
+++ src/worker/macos/recent_file_cache.h
@@ -5,21 +5,34 @@
 #include <chrono>
 #include <unordered_map>
 #include <map>
+#include <memory>
+#include <sys/stat.h>
 
 #include "../../message.h"
 
+struct CacheEntry {
+  CacheEntry(std::string path, EntryKind entry_kind, ino_t inode, off_t size);
+
+  std::string path;
+  EntryKind entry_kind;
+  ino_t inode;
+  off_t size;
+  std::chrono::time_point<std::chrono::steady_clock> last_seen;
+};
+
 class RecentFileCache {
 public:
-  void does_exist(const std::string &path, const EntryKind &entryKind);
+  void does_exist(const std::string &path, EntryKind entry_kind, ino_t inode, off_t size);
   void does_not_exist(const std::string &path);
 
-  bool has_been_seen(const std::string &path, const EntryKind &entryKind);
-  EntryKind last_known_kind(const std::string &path);
+  std::shared_ptr<CacheEntry> at_path(const std::string &path);
+  std::shared_ptr<CacheEntry> at_inode(const ino_t inode);
 
   void purge();
 private:
-  std::unordered_map<std::string, EntryKind> seen_paths;
-  std::multimap<std::chrono::time_point<std::chrono::steady_clock>, std::string> by_timestamp;
+  std::unordered_map<std::string, std::shared_ptr<CacheEntry>> by_path;
+  std::unordered_map<ino_t, std::shared_ptr<CacheEntry>> by_inode;
+  std::multimap<std::chrono::time_point<std::chrono::steady_clock>, std::shared_ptr<CacheEntry>> by_timestamp;
 };
 
 #endif
