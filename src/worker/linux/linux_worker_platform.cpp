85d2ec1aed4042aa1ae1813e1d8e5715f91006d6
Ash Wilson <smashwilson@github.com>
2017-07-23 15:07:29 -0400

    Implement WorkerPlatform::for_worker

--- src/worker/linux/linux_worker_platform.cpp
+++ src/worker/linux/linux_worker_platform.cpp
@@ -1,13 +1,18 @@
 #include <string>
+#include <memory>
 
 #include "../worker_platform.h"
+#include "../worker_thread.h"
 #include "../../message.h"
 #include "../../log.h"
 
 using std::string;
+using std::unique_ptr;
 
 class LinuxWorkerPlatform : public WorkerPlatform {
 public:
+  LinuxWorkerPlatform(WorkerThread *thread) : WorkerPlatform(thread) {};
+
   void wake() override
   {
     //
@@ -27,4 +32,9 @@ public:
   {
     //
   }
+}
+
+unique_ptr<WorkerPlatform> WorkerPlatform::for_worker(WorkerThread *thread)
+{
+  return unique_ptr<WorkerPlatform>(new LinuxWorkerPlatform(thread));
 }
