33ca7b86b176545a16696409dfe593a18f63b1ff
Ash Wilson <smashwilson@github.com>
2017-07-24 19:19:40 -0400

    Collect status data into an open struct

---
+++ src/status.h
@@ -0,0 +1,25 @@
+#ifndef STATUS_H
+#define STATUS_H
+
+#include <string>
+#include <iostream>
+
+// Summarize the module's health. This includes information like the health of all Errable and SyncErrable
+// resources and the sizes of internal queues and buffers.
+class Status {
+public:
+  // Main thread
+  size_t pending_callback_count;
+  size_t channel_callback_count;
+
+  // Worker thread
+  std::string worker_thread_ok;
+  size_t worker_in_size;
+  std::string worker_in_ok;
+  size_t worker_out_size;
+  std::string worker_out_ok;
+};
+
+std::ostream &operator<<(std::ostream &out, const Status &status);
+
+#endif
