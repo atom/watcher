5411cb46be2860ea00b6b9f9099d61ec9f76708c
Ash Wilson <smashwilson@gmail.com>
2017-08-23 12:17:10 -0400

    Extract to_wchar() as a counterpoint to to_utf8()

--- src/worker/windows/windows_worker_platform.cpp
+++ src/worker/windows/windows_worker_platform.cpp
@@ -37,6 +37,8 @@ static void CALLBACK event_helper(DWORD error_code, DWORD num_bytes, LPOVERLAPPE
 
 static Result<string> to_utf8(const wstring &in);
 
+static Result<wstring> to_wchar(const string &in);
+
 template < class V = void* >
 static Result<V> windows_error_result(const string &prefix);
 
@@ -443,33 +445,62 @@ Result<string> to_utf8(const wstring &in)
     CP_UTF8, // code page
     0, // flags
     in.data(), // source string
-    in.size() / sizeof(WCHAR), // source string length
+    in.size(), // source string length
     nullptr, // destination string, null to measure
     0, // destination string length
     nullptr, // default char
     nullptr  // used default char
   );
   if (!len) {
-    return windows_error_result<string>("Unable to measure path as UTF-8");
+    return windows_error_result<string>("Unable to measure string as UTF-8");
   }
 
-  char *out = new char[len];
+  unique_ptr<char[]> payload(new char[len]);
   size_t copied = WideCharToMultiByte(
     CP_UTF8, // code page
     0, // flags
     in.data(), // source string
-    in.size() / sizeof(WCHAR), // source string length
-    out, // destination string
+    in.size(), // source string length
+    payload.get(), // destination string
     len, // destination string length
     nullptr, // default char
     nullptr // used default char
   );
   if (!copied) {
-    delete [] out;
-    return windows_error_result<string>("Unable to convert path to UTF-8");
+    return windows_error_result<string>("Unable to convert string to UTF-8");
   }
 
-  return ok_result(string{out, len});
+  return ok_result(string(payload.get(), len));
+}
+
+Result<wstring> to_wchar(const string &in)
+{
+  size_t wlen = MultiByteToWideChar(
+    CP_UTF8, // code page
+    0, // flags
+    in.c_str(), // source string data
+    -1, // source string length (null-terminated)
+    0, // output buffer
+    0 // output buffer size
+  );
+  if (wlen == 0) {
+    return windows_error_result<wstring>("Unable to measure string as wide string");
+  }
+
+  unique_ptr<WCHAR[]> payload(new WCHAR[wlen]);
+  size_t conv_success = MultiByteToWideChar(
+    CP_UTF8, // code page
+    0, // flags,
+    in.c_str(), // source string data
+    -1, // source string length (null-terminated)
+    payload.get(), // output buffer
+    wlen // output buffer size (in bytes)
+  );
+  if (!conv_success) {
+    return windows_error_result<wstring>("Unable to convert string to wide string");
+  }
+
+  return ok_result(wstring(payload.get(), wlen - 1));
 }
 
 template < class V >
