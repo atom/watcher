779817ddff0319b83eb8ea189de1d110ffda723e
Ash Wilson <smashwilson@github.com>
2017-09-26 15:21:49 -0400

    Refactor common command handling into Thread

--- src/thread.cpp
+++ src/thread.cpp
@@ -7,12 +7,15 @@
 
 #include "thread.h"
 #include "message.h"
+#include "result.h"
+#include "log.h"
 
 using std::string;
 using std::function;
 using std::unique_ptr;
 using std::vector;
 using std::move;
+using std::endl;
 
 void thread_callback_helper(void *arg)
 {
@@ -20,8 +23,19 @@ void thread_callback_helper(void *arg)
   (*bound_fn)();
 }
 
+using CommandHandler = Result<> (Thread::*)(const CommandPayload*, Thread::CommandOutcome&);
+const CommandHandler command_handlers[] = {
+  [COMMAND_ADD]=&Thread::handle_add_command,
+  [COMMAND_REMOVE]=&Thread::handle_remove_command,
+  [COMMAND_LOG_FILE]=&Thread::handle_log_file_command,
+  [COMMAND_LOG_STDERR]=&Thread::handle_log_stderr_command,
+  [COMMAND_LOG_STDOUT]=&Thread::handle_log_stdout_command,
+  [COMMAND_LOG_DISABLE]=&Thread::handle_log_disable_command
+};
+
 Result<> Thread::run()
 {
+  mark_starting();
   int err;
 
   err = uv_thread_create(&uv_handle, thread_callback_helper, &work_fn);
@@ -40,8 +54,18 @@ Result<> Thread::send(Message &&message)
   Result<> qr = in.enqueue(move(message));
   if (qr.is_error()) return qr;
 
-  Result<> wr = wake();
-  if (wr.is_error()) return wr;
+  if (is_running() || is_stopping()) {
+    Result<> wr = wake();
+    if (wr.is_error()) return wr;
+  }
+
+  if (is_stopped()) {
+    bool trigger = should_trigger_run(message);
+    if (trigger) {
+      Result<> rr = run();
+      if (rr.is_error()) return rr;
+    }
+  }
 
   return ok_result();
 }
@@ -68,34 +92,91 @@ Result<> Thread::emit(Message &&message)
   return ok_result();
 }
 
-Result< unique_ptr<vector<Message>> > Thread::process_all()
+Result<size_t> Thread::handle_commands()
 {
-  if (!is_healthy()) return health_err_result< unique_ptr<vector<Message>> >();
+  Result< unique_ptr<vector<Message>> > pr = in.accept_all();
+  if (pr.is_error()) {
+    return pr.propagate<size_t>();
+  }
+  unique_ptr<vector<Message>> &accepted = pr.get_value();
+  if (!accepted) {
+    // No command messages to accept.
+    return ok_result(static_cast<size_t>(0));
+  }
 
-  return in.accept_all();
+  vector<Message> acks;
+  acks.reserve(accepted->size());
+
+  for (Message &message : *accepted) {
+    const CommandPayload *command = message.as_command();
+    if (!command) {
+      LOGGER << "Received unexpected non-command message " << message << "." << endl;
+      continue;
+    }
+
+    CommandOutcome outcome = {
+      true, // ack
+      true, // success
+    };
+    string m = "";
+
+    CommandHandler handler = command_handlers[command->get_action()];
+    if (handler == nullptr) {
+      handler = &Thread::handle_unknown_command;
+    }
+    Result<> hr = (this->*handler)(command, outcome);
+    if (hr.is_error()) {
+      LOGGER << "Reporting command handler error: " << hr << "." << endl;
+      outcome.success = false;
+      m = move(hr.get_error());
+    }
+
+    if (outcome.ack) {
+      AckPayload ack(command->get_id(), command->get_channel_id(), outcome.success, move(m));
+      Message response(move(ack));
+      acks.push_back(move(response));
+    }
+  }
+
+  return emit_all(acks.begin(), acks.end()).propagate<size_t>(accepted->size());
 }
 
-Result<> Thread::wake()
+Result<> Thread::handle_add_command(const CommandPayload *payload, CommandOutcome &outcome)
 {
+  return handle_unknown_command(payload, outcome);
+}
+
+Result<> Thread::handle_remove_command(const CommandPayload *payload, CommandOutcome &outcome)
+{
+  return handle_unknown_command(payload, outcome);
+}
+
+Result<> Thread::handle_log_file_command(const CommandPayload *payload, CommandOutcome &outcome)
+{
+  Logger::to_file(payload->get_root().c_str());
   return ok_result();
 }
 
-string Thread::get_in_queue_error()
+Result<> Thread::handle_log_stderr_command(const CommandPayload *payload, CommandOutcome &outcome)
 {
-  return in.get_error();
+  Logger::to_stderr();
+  return ok_result();
 }
 
-size_t Thread::get_in_queue_size()
+Result<> Thread::handle_log_stdout_command(const CommandPayload *payload, CommandOutcome &outcome)
 {
-  return in.size();
+  Logger::to_stdout();
+  return ok_result();
 }
 
-string Thread::get_out_queue_error()
+Result<> Thread::handle_log_disable_command(const CommandPayload *payload, CommandOutcome &outcome)
 {
-  return out.get_error();
+  Logger::to_stderr();
+  return ok_result();
 }
 
-size_t Thread::get_out_queue_size()
+Result<> Thread::handle_unknown_command(const CommandPayload *payload, CommandOutcome &outcome)
 {
-  return out.size();
+  LOGGER << "Received command with unexpected action " << *payload << "." << endl;
+  return ok_result();
 }
--- src/thread.h
+++ src/thread.h
@@ -1,6 +1,7 @@
 #ifndef THREAD_H
 #define THREAD_H
 
+#include <atomic>
 #include <string>
 #include <memory>
 #include <utility>
@@ -24,7 +25,8 @@ public:
     in(name + " input queue"),
     out(name + " output queue"),
     main_callback{main_callback},
-    work_fn{std::bind(std::mem_fn(fn), self)}
+    work_fn{std::bind(std::mem_fn(fn), self)},
+    state{State::STOPPED}
   {
     //
   };
@@ -41,8 +43,25 @@ public:
     Result<> qr = in.enqueue_all(begin, end);
     if (qr.is_error()) return qr;
 
-    Result<> wr = wake();
-    if (wr.is_error()) return wr;
+    if (is_running() || is_stopping()) {
+      Result<> wr = wake();
+      if (wr.is_error()) return wr;
+    }
+
+    if (is_stopped()) {
+      bool trigger = false;
+      for (auto it = begin; it != end; ++it) {
+        if (should_trigger_run(*it)) {
+          trigger = true;
+          break;
+        }
+      }
+
+      if (trigger) {
+        Result<> rr = run();
+        if (rr.is_error()) return rr;
+      }
+    }
 
     return ok_result();
   }
@@ -51,8 +70,24 @@ public:
 
   virtual void collect_status(Status &status) = 0;
 
+  struct CommandOutcome {
+    bool ack;
+    bool success;
+  };
+
+  virtual Result<> handle_add_command(const CommandPayload *payload, CommandOutcome &outcome);
+  virtual Result<> handle_remove_command(const CommandPayload *payload, CommandOutcome &outcome);
+  Result<> handle_log_file_command(const CommandPayload *payload, CommandOutcome &outcome);
+  Result<> handle_log_stderr_command(const CommandPayload *payload, CommandOutcome &outcome);
+  Result<> handle_log_stdout_command(const CommandPayload *payload, CommandOutcome &outcome);
+  Result<> handle_log_disable_command(const CommandPayload *payload, CommandOutcome &outcome);
+  Result<> handle_unknown_command(const CommandPayload *payload, CommandOutcome &outcome);
+
 protected:
-  virtual Result<> wake();
+  virtual Result<> wake()
+  {
+    return ok_result();
+  }
 
   Result<> emit(Message &&message);
 
@@ -72,14 +107,33 @@ protected:
     return ok_result();
   }
 
-  Result< std::unique_ptr<std::vector<Message>> > process_all();
+  Result<size_t> handle_commands();
 
-  std::string get_in_queue_error();
-  size_t get_in_queue_size();
-  std::string get_out_queue_error();
-  size_t get_out_queue_size();
+  void mark_stopped() { state.store(State::STOPPED); }
+  void mark_starting() { state.store(State::STARTING); }
+  void mark_running() { state.store(State::RUNNING); }
+  void mark_stopping() { state.store(State::STOPPING); }
+
+  bool is_starting() { return state.load() == State::STARTING; }
+  bool is_running() { return state.load() == State::RUNNING; }
+  bool is_stopped() { return state.load() == State::STOPPED; }
+  bool is_stopping() { return state.load() == State::STOPPING; }
+
+  virtual bool should_trigger_run(Message &message) { return false; }
+
+  std::string get_in_queue_error() { return in.get_error(); }
+  size_t get_in_queue_size() { return in.size(); }
+  std::string get_out_queue_error() { return out.get_error(); }
+  size_t get_out_queue_size() { return out.size(); }
 
 private:
+  enum State {
+    STOPPED,
+    STARTING,
+    RUNNING,
+    STOPPING
+  };
+
   Queue in;
   Queue out;
 
@@ -87,6 +141,8 @@ private:
 
   uv_thread_t uv_handle;
   std::function<void()> work_fn;
+
+  std::atomic<State> state;
 };
 
 #endif
