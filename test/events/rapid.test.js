926fccf1e5c2a2c5bb3a491f37f2fdc9c53c029c
Ash Wilson <smashwilson@github.com>
2017-10-24 15:45:09 -0400

    MacOS will never be able to correlate the rename without an inode

--- test/events/rapid.test.js
+++ test/events/rapid.test.js
@@ -54,10 +54,18 @@ const {EventMatcher} = require('../matcher');
         await fs.writeFile(originalPath, 'contents\n')
         await fs.rename(originalPath, finalPath)
 
-        await until('both events arrive', matcher.orderedEvents(
-          {action: 'created', kind: 'file', path: originalPath},
-          {action: 'renamed', kind: 'file', oldPath: originalPath, path: finalPath}
-        ))
+        if (process.platform === 'darwin') {
+          await until('creation and deletion events arrive', matcher.orderedEvents(
+            {action: 'created', kind: 'file', path: originalPath},
+            {action: 'deleted', kind: 'file', path: originalPath},
+            {action: 'created', kind: 'file', path: finalPath}
+          ))
+        } else {
+          await until('creation and rename events arrive', matcher.orderedEvents(
+            {action: 'created', kind: 'file', path: originalPath},
+            {action: 'renamed', kind: 'file', oldPath: originalPath, path: finalPath}
+          ))
+        }
       })
     }
 
