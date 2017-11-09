72307914ec73cbecbb874f3d79c98a651a878b2b
Ash Wilson <smashwilson@github.com>
2017-11-09 10:05:31 -0500

    Report errors from static Logger methods

--- src/log.h
+++ src/log.h
@@ -1,31 +1,41 @@
 #ifndef LOG_H
 #define LOG_H
 
+#include <memory>
 #include <ostream>
 #include <string>
 
 class Logger
 {
 public:
+  static Logger *current();
+
+  static std::string to_file(const char *filename);
+
+  static std::string to_stderr();
+
+  static std::string to_stdout();
+
+  static std::string disable();
+
   Logger() = default;
-  Logger(const Logger &) = delete;
-  Logger(Logger &&) = delete;
+
   virtual ~Logger() = default;
 
-  static Logger *current();
-  static void to_file(const char *filename);
-  static void to_stderr();
-  static void to_stdout();
-  static void disable();
-
   virtual Logger *prefix(const char *file, int line) = 0;
+
   virtual std::ostream &stream() = 0;
 
+  virtual std::string get_error() const { return ""; }
+
+  Logger(const Logger &) = delete;
+  Logger(Logger &&) = delete;
   Logger &operator=(const Logger &) = delete;
   Logger &operator=(Logger &&) = delete;
 };
 
 std::string plural(long quantity, const std::string &singular_form, const std::string &plural_form);
+
 std::string plural(long quantity, const std::string &singular_form);
 
 #define LOGGER (Logger::current()->prefix(__FILE__, __LINE__)->stream())
