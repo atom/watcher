635fc82a18c9ff48c3b8b8185251629d49fbd653
Ash Wilson <smashwilson@gmail.com>
2017-11-21 15:30:26 -0500

    Enact side effects immediately

--- src/worker/linux/side_effect.cpp
+++ src/worker/linux/side_effect.cpp
@@ -1,3 +1,4 @@
+#include <memory>
 #include <string>
 #include <utility>
 #include <vector>
@@ -9,6 +10,7 @@
 #include "watch_registry.h"
 
 using std::move;
+using std::shared_ptr;
 using std::string;
 using std::vector;
 
@@ -17,11 +19,11 @@ void SideEffect::track_subdirectory(string subdir, ChannelID channel_id)
   subdirectories.emplace_back(move(subdir), channel_id);
 }
 
-void SideEffect::enact_in(WatchRegistry *registry, MessageBuffer &messages)
+void SideEffect::enact_in(const shared_ptr<WatchedDirectory> &parent, WatchRegistry *registry, MessageBuffer &messages)
 {
   for (Subdirectory &subdir : subdirectories) {
     vector<string> poll_roots;
-    Result<> r = registry->add(subdir.channel_id, subdir.path, true, poll_roots);
+    Result<> r = registry->add(subdir.channel_id, parent, subdir.basename, true, poll_roots);
     if (r.is_error()) messages.error(subdir.channel_id, string(r.get_error()), false);
 
     for (string &poll_root : poll_roots) {
