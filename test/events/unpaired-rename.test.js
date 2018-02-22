2b5a27079721b86121723a3d9c4b7cd0a8948965
Ash Wilson <smashwilson@github.com>
2018-02-21 19:05:01 -0800

    Create a test to capture the "move out and in" case

--- test/events/unpaired-rename.test.js
+++ test/events/unpaired-rename.test.js
@@ -57,5 +57,28 @@ const {EventMatcher} = require('../matcher');
         {action: 'deleted', kind: 'file', path: insideFile}
       ))
     })
+
+    it('when a file is renamed out of, then back into, the watch root', async function () {
+      const outsideFile = fixture.fixturePath('file.txt')
+      const insideFile = fixture.watchPath('file.txt')
+
+      await fs.writeFile(insideFile, 'contents')
+
+      await until('the creation event arrives', matcher.allEvents(
+        {action: 'created', kind: 'file', path: insideFile}
+      ))
+      matcher.reset()
+
+      await fs.rename(insideFile, outsideFile)
+      await until('the deletion event arrives', matcher.allEvents(
+        {action: 'deleted', kind: 'file', path: insideFile}
+      ))
+      matcher.reset()
+
+      await fs.rename(outsideFile, insideFile)
+      await until('the re-creation event arrives', matcher.allEvents(
+        {action: 'created', kind: 'file', path: insideFile}
+      ))
+    })
   })
 })
